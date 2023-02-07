51f7f0716b0499135b0df277d7148f92844d1379
JLCodeSource <JLCodeSource@gmail.com>
2023-02-07 17:47:32 +0000

    Tidying

--- process_async_processed.sh
+++ process_async_processed.sh
@@ -3,24 +3,23 @@
 # Purpose: This script processes a list of files in the async_processed dataset
 
 # Vars
-test=1
+test=
 user=nmr
 dir="/home/JLCodeSource/shell/process_async_ds"
 outdir=$dir/out
-testfile=p1testout.out
-file=p1test_async_processed_files.out
+testfile=test_processed_files.out
+file=async_processed_files.out
 
 
 # Consts
 ftp='ftp://'
 ftp_ip_and_port=([0-9]{1,3}\.){3}[0-9]{1,3}:2121
 backup_guid='[a-fA-F0-9]{8}(-[a-fA-F0-9]{8}){5}'
-fanstagingtype=("fan_c2:" "fan_c1:" "fan_cold" "fan")
-stagingpath=("/data3/staging" "/data2/staging" "/data1/staging" "/mb/FAN")
+fanstaging_type=("fan_c2:" "fan_c1:" "fan_c0:" "fan:")
+staging_path=("/data3/staging" "/data2/staging" "/data1/staging" "/mb/FAN")
 sites=("site1" "site2")
 segment=("41" "49")
 declare -A nodes
-declare -A fan_to_staging
 ipstart=101
 
 
@@ -34,7 +33,7 @@ file="$testfile"
 fi
 
 if [ ! -d $outdir ]; then
-mkdir $outdir
+mkdir -p $outdir
 fi
 
 ## --- Creating nodes = ip to node hashmap
@@ -59,74 +58,68 @@ echo "---Hashmap nodes"
 echo "---Hashmap size ${#nodes[@]}" 
 for ip in "${!nodes[@]}"; do echo "$ip - ${nodes[$ip]}"; done
 
-## --- Creating fan_to_staging hashmap
-
-for i in {0..3}; 
-do
-    fan_to_staging["${fanstagingtype[i]}"]=${stagingpath[i]}
-done
-echo "---Hashmap fan_to_staging"
-for staging in "${!fan_to_staging[@]}"; do echo $staging - ${fan_to_staging[$staging]}; done
-
-
 # --- Data cleansing
 function data_cleanse_file () {
 
-    # -- Drop Unnecessary Files
+    # Working File
+    local file=$1
 
-    echo Total Files: $(wc -l $1)
+    # -- Drop Unnecessary Files
+    # N.B. Remove top line
+    echo Total Files: $(wc -l $file)
 
     ## Drop files that aren't backup guids
-    cat $1 | grep -v -E "$backup_guid" > $outdir/dumped_non_backup_guids.out
-    cat $1 | grep -E "$backup_guid" > tmp_file
-    mv tmp_file $1
-    echo Total Files After Removing Files without Backup GUID name: $(wc -l $1)
+    cat $file | grep -v -E "$backup_guid" > $outdir/dumped_non_backup_guids.out
+    cat $file | grep -E "$backup_guid" > tmp_file
+    mv tmp_file $file
+    echo Total Files After Removing Files without Backup GUID name: $(wc -l $file)
 
     ## Drop files with a hash
-    awk -F'|' '$8!="" {print}' $1 > $outdir/dumped_files_with_hash.out
-    awk -F'|' '$8=="" {print}' $1 > tmp_file
-    mv tmp_file $1
-    echo Total Files After Removing Files with a Hash: $(wc -l $1)
+    awk -F'|' '$8!="" {print}' $file > $outdir/dumped_files_with_hash.out
+    awk -F'|' '$8=="" {print}' $file > tmp_file
+    mv tmp_file $file
+    echo Total Files After Removing Files with a Hash: $(wc -l $file)
 
     ## Drop files with no fanip
-    awk -F'|' '$3=="null" { print }' $1 > $outdir/dumped_files_with_no_fanip.out 
-    awk -F'|' '$3!="null" { print }' $1 > tmp_file
-    mv tmp_file $1
-    echo Total Files After Removing Files with No FanIP: $(wc -l $1)
+    awk -F'|' '$3=="null" { print }' $file > $outdir/dumped_files_with_no_fanip.out 
+    awk -F'|' '$3!="null" { print }' $file > tmp_file
+    mv tmp_file $file
+    echo Total Files After Removing Files with No FanIP: $(wc -l $file)
 
     ## Drop files with no fanuri
-    awk -F'|' '$4=="null" { print }' $1 > $outdir/dumped_files_with_no_fanuri.out
-    awk -F'|' '$4!="null" { print }' $1 > tmp_file
-    mv tmp_file $1
-    echo Total Files After Removing Files with No FanURI: $(wc -l $1)
+    awk -F'|' '$4=="null" { print }' $file > $outdir/dumped_files_with_no_fanuri.out
+    awk -F'|' '$4!="null" { print }' $file > tmp_file
+    mv tmp_file $file
+    echo Total Files After Removing Files with No FanURI: $(wc -l $file)
 
     ## Drop files with Extracted
-    cat $1 | grep "backupkv Extracted" > $outdir/dumped_files_with_extracted.out
-    cat $1 | grep -v "backupkv Extracted" > tmp_file
-    mv tmp_file $1
-    echo Total Files After Removing Files with Backup Info: $(wc -l $1)
+    cat $file | grep "backupkv Extracted" > $outdir/dumped_files_with_extracted.out
+    cat $file | grep -v "backupkv Extracted" > tmp_file
+    mv tmp_file $file
+    echo Total Files After Removing Files with Backup Info: $(wc -l $file)
 
 
     # -- Tidy FanURI
 
     ## Strip ftp
-    sed -i "s;$ftp;;g" $1
+    sed -i "s;$ftp;;g" $file
 
     ## Strip user
-    sed -i "s;:$user;;g" $1
+    sed -i "s;$user;;g" $file
 
     ## Strip ip
-    sed -i -E "s;$ftp_ip_and_port;;g" $1
+    sed -i -E "s;$ftp_ip_and_port;;g" $file
 
     # Switch fan/fan_cold[1,2] to staging path
-    for swap in "${!fan_to_staging[@]}"; do
-        echo swap: $swap fan_to_staging: ${fan_to_staging[$swap]}
-        sed -i "s;$swap;${fan_to_staging[$swap]};g" $1
+    # Order
+    for i in {0..3}; do
+        echo swap: "${fanstaging_type[$i]}" fan_to_staging: "${staging_path[$i]}"
+        sed -i "s;${fanstaging_type[$i]};${staging_path[$i]};g" $file
     done
 
     # Reorder & drop unnecessary fields
-    awk -F'|' '{ print $4 "|" $2 "|" $5 "|" $7 "|" $3 }' $1 > tmp_file
-    mv tmp_file $1
+    awk -F'|' '{ print $4 "|" $2 "|" $5 "|" $7 "|" $3 }' $file > tmp_file
+    mv tmp_file $file
 
     echo Cleaned File
 }
@@ -134,10 +127,12 @@ function data_cleanse_file () {
 # --- Splitting by node
 function node_split () {
 
+local file=$1
+
 for ip in "${!nodes[@]}"; 
 do 
     echo "--- $ip"
-    cat $1 | grep $ip > tmp_file
+    cat $file | grep $ip > tmp_file
     awk -F'|' '$5="$ip" { print $1 "|" $2 "|" $3 "|" $4 }' tmp_file > "$outdir/$ip".out
     echo $(wc -l "$outdir/$ip.out")
 done
@@ -163,5 +158,3 @@ clean_up
 }
 
 main
-
-#find $pwd/shell/process_async_processed/ -print0 -printf "|%TY-%Tm-%Td %TT|%b|id\n"
