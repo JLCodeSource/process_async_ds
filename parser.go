ca9c7380bdb94a553de06d7c858dbaa39208b93d
JLCodeSource <JLCodeSource@gmail.com>
2023-02-24 22:58:57 +0000

    Added extraction of fileMetap.ta

--- parser.go
+++ parser.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"io/fs"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -31,10 +33,27 @@ func parseFile(fsys fs.FS, f string, logger *logrus.Logger) []string {
 }
 
 func parseLine(line string, logger *logrus.Logger) File {
-	vals := strings.SplitAfter(line, "|")
-	for i := 0; i < len(vals); i++ {
-		vals[i] = vals[i][0 : len(vals[i])-1]
+	fileMetadata := strings.SplitAfter(line, "|")
+	for i := 0; i < len(fileMetadata); i++ {
+		if i < (len(fileMetadata) - 1) {
+			fileMetadata[i] = fileMetadata[i][0 : len(fileMetadata[i])-1]
+		}
 	}
-	file := File{path: vals[0]}
+
+	path := fileMetadata[0]
+
+	loc, _ := time.LoadLocation("America/New_York")
+	datestring := fileMetadata[1]
+	datetime, _ := time.ParseInLocation(time.UnixDate, datestring, loc)
+
+	size, _ := strconv.ParseInt(fileMetadata[2], 10, 64)
+
+	id := fileMetadata[3]
+
+	file := File{
+		path:       path,
+		createTime: datetime,
+		size:       size,
+		id:         id}
 	return file
 }
