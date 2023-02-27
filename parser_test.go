5084f91479d246c257b0dbe85e6c4b21ff2991dc
JLCodeSource <JLCodeSource@gmail.com>
2023-02-27 12:25:54 +0000

    Table tests for parseFile (not Line)

--- parser_test.go
+++ parser_test.go
@@ -7,6 +7,8 @@ import (
 	"testing/fstest"
 
 	"bou.ke/monkey"
+	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -18,49 +20,62 @@ const (
 )
 
 var (
-	multilineOut = []string{
+	onelineParsed = []string{"/data2/staging/05043fe1-00000006-2f8630d0-608630d0-67d25000-ab66ac56{gbtmp-FD40CB70A63D11EBAB7FB02628E0E270}|Sun Apr 25 23:17:53 EDT 2021|0|95BA50C0A64211EB8B73B026285E5DA0"}
+
+	multilineParsed = []string{
 		"/data2/staging/05043fe1-00000006-2f8630d0-608630d0-67d25000-ab66ac56{gbtmp-FD40CB70A63D11EBAB7FB02628E0E270}|Sun Apr 25 23:17:53 EDT 2021|0|95BA50C0A64211EB8B73B026285E5DA0",
 		"/data2/staging/03bdd706-00000006-f8836565-60836565-2e095000-ab66ac56{gbtmp-E9DE7470A49311EBAB7FB02628E0E270}|Fri Apr 23 20:29:14 EDT 2021|0|24A80BC0A49411EB9275B026285E5440",
 		"/data1/staging/ffbb5588-00000006-a08893b2-608893b2-32645000-ee50a856{gbtmp-113E8140A7AA11EB94CCB02628E0E270}|Tue Apr 27 18:44:04 EDT 2021|0|55670DD0A7AD11EB985CB026285E5410"}
 )
 
 func TestParseFile(t *testing.T) {
-	t.Run("parse file", func(t *testing.T) {
-		testLogger, hook := setupLogs(t)
-		fs := fstest.MapFS{
-			"path/processed_files.out": {
-				Data: []byte(oneline)},
+
+	t.Run("test parseFile", func(t *testing.T) {
+		var testLogger *logrus.Logger
+		var hook *test.Hook
+		fs := fstest.MapFS{}
+
+		parsingTests := []struct {
+			name    string
+			content string
+			want    []string
+			log     string
+		}{
+			{
+				name:    "parsefile",
+				content: oneline,
+				want:    onelineParsed,
+				log:     "Processing: ",
+			},
+			{
+				name:    "parse multiline file",
+				content: multiline,
+				want:    multilineParsed,
+				log:     "Processing: ",
+			},
 		}
-		onelineParsed := oneline[0 : len(oneline)-1]
-		out := parseFile(fs, "path/processed_files.out", testLogger)
 
-		got := out[0]
-		want := onelineParsed
-		assertCorrectString(t, got, want)
+		for _, tt := range parsingTests {
+			t.Run(tt.name, func(t *testing.T) {
+				testLogger, hook = setupLogs(t)
+				fs = fstest.MapFS{
+					"path/processed_files.out": {
+						Data: []byte(tt.content)},
+				}
 
-		gotLogMsg := hook.LastEntry().Message
-		wantLogMsg := "Processing: " + out[0]
-		assertCorrectString(t, gotLogMsg, wantLogMsg)
-	})
+				got := parseFile(fs, "path/processed_files.out", testLogger)
 
-	t.Run("parse multiline-file", func(t *testing.T) {
-		testLogger, hook := setupLogs(t)
-		fs := fstest.MapFS{
-			"path/processed_files.out": {
-				Data: []byte(multiline)},
-		}
-		got := parseFile(fs, "path/processed_files.out", testLogger)
-		logs := hook.AllEntries()
+				logs := hook.AllEntries()
 
-		want := multilineOut
+				for i := 0; i < len(tt.want); i++ {
+					assertCorrectString(t, got[i], tt.want[i])
 
-		for i := 0; i < len(want); i++ {
-			assertCorrectString(t, got[i], want[i])
+					gotLogMsg := logs[i].Message
+					wantLogMsg := "Processing: " + got[i]
+					assertCorrectString(t, gotLogMsg, wantLogMsg)
 
-			gotLogMsg := logs[i].Message
-			wantLogMsg := "Processing: " + got[i]
-			assertCorrectString(t, gotLogMsg, wantLogMsg)
-
+				}
+			})
 		}
 	})
 
@@ -104,10 +119,10 @@ func TestParseLine(t *testing.T) {
 			log  string
 		}{
 			{
-				"verify path",
-				workingFile.path,
-				"/data2/staging/05043fe1-00000006-2f8630d0-608630d0-67d25000-ab66ac56{gbtmp-FD40CB70A63D11EBAB7FB02628E0E270}",
-				"path: ",
+				name: "verify path",
+				got:  workingFile.path,
+				want: "/data2/staging/05043fe1-00000006-2f8630d0-608630d0-67d25000-ab66ac56{gbtmp-FD40CB70A63D11EBAB7FB02628E0E270}",
+				log:  "path: ",
 			},
 			{
 				"verify createTime",
