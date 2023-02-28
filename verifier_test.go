6e3f1f9561e8aa09f81a179eb4899f9ff36ea73a
JLCodeSource <JLCodeSource@gmail.com>
2023-02-28 16:19:36 +0000

    Refactored verify tests

--- verifier_test.go
+++ verifier_test.go
@@ -6,25 +6,31 @@ import (
 	"testing"
 	"time"
 
+	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestVerifyFileIP(t *testing.T) {
+	// setup logger
+	var testLogger *logrus.Logger
+	var hook *test.Hook
+
+	// setup file
+	var file File
+
+	// setup server ip
+	hostname, _ := os.Hostname()
+	ips, _ := net.LookupIP(hostname)
+	// set incorrect ip
+	ip := net.ParseIP("192.168.101.1")
 
 	t.Run("returns true if ip is same as the current machine", func(t *testing.T) {
-		hostname, err := os.Hostname()
-		if err != nil {
-			panic(err)
-		}
-		ips, err := net.LookupIP(hostname)
-		if err != nil {
-			panic(err)
-		}
-		file := File{
+		file = File{
 			smbName: "file.txt",
 			fanIP:   ips[0],
 		}
-		testLogger, hook := setupLogs(t)
+		testLogger, hook = setupLogs(t)
 		assert.True(t, file.verifyIP(ips[0], testLogger))
 		gotLogMsg := hook.LastEntry().Message
 		wantLogMsg := "file.txt ip:" + file.fanIP.String() + " matches comparison ip:" + ips[0].String()
@@ -33,20 +39,11 @@ func TestVerifyFileIP(t *testing.T) {
 
 	})
 	t.Run("returns false if ip is not the same as the current machine", func(t *testing.T) {
-		hostname, err := os.Hostname()
-		if err != nil {
-			panic(err)
-		}
-		ips, err := net.LookupIP(hostname)
-		if err != nil {
-			panic(err)
-		}
-		file := File{
+		file = File{
 			smbName: "file.txt",
 			fanIP:   ips[0],
 		}
-		testLogger, hook := setupLogs(t)
-		ip := net.ParseIP("192.168.101.1")
+		testLogger, hook = setupLogs(t)
 		assert.False(t, file.verifyIP(ip, testLogger))
 
 		gotLogMsg := hook.LastEntry().Message
@@ -59,17 +56,27 @@ func TestVerifyFileIP(t *testing.T) {
 }
 
 func TestVerifyTimeLimit(t *testing.T) {
+	// setup logger
+	var testLogger *logrus.Logger
+	var hook *test.Hook
+
+	// setup file
+	var file File
+
+	// setup limit
+	var limit time.Time
+
+	days := time.Duration(15)
+	hours := time.Duration(days * 24)
+	now := time.Now()
 
 	t.Run("returns true if file.date is after time limit", func(t *testing.T) {
-		file := File{
+		file = File{
 			smbName:    "file.txt",
-			createTime: time.Now(),
+			createTime: now,
 		}
-		days := time.Duration(15)
-		hours := time.Duration(days * 24)
-		now := time.Now()
-		limit := now.Add(-((hours) * time.Hour))
-		testLogger, hook := setupLogs(t)
+		testLogger, hook = setupLogs(t)
+		limit = now.Add(-((hours) * time.Hour))
 		assert.True(t, file.verifyTimeLimit(limit, testLogger))
 		gotLogMsg := hook.LastEntry().Message
 		wantLogMsg := "file.txt createTime:" + file.createTime.String() + " is after timelimit:" + limit.String()
@@ -78,20 +85,58 @@ func TestVerifyTimeLimit(t *testing.T) {
 
 	})
 	t.Run("returns false if file.date is before time limit", func(t *testing.T) {
-		file := File{
+		file = File{
 			smbName:    "file.txt",
-			createTime: time.Now(),
+			createTime: now,
 		}
-		limit := file.createTime.Add(24 * time.Hour)
+		limit = now.Add(24 * time.Hour)
 		testLogger, hook := setupLogs(t)
 		assert.False(t, file.verifyTimeLimit(limit, testLogger))
 
 		gotLogMsg := hook.LastEntry().Message
 		wantLogMsg := ("file.txt createTime:" + file.createTime.String() + " is before timelimit:" +
-			limit.String() +
-			"; skipping file")
+			limit.String() + "; skipping file")
 
 		assertCorrectString(t, gotLogMsg, wantLogMsg)
 	})
 
 }
+
+/*
+Need to work this out for the future
+type function func(File, interface{}, *logrus.Logger) bool
+
+func TestVerify(t *testing.T) {
+	// setup logger
+	var testLogger *logrus.Logger
+	var hook *test.Hook
+
+	// setup file
+	var file File
+
+	// setup server ip
+	hostname, _ := os.Hostname()
+	ips, _ := net.LookupIP(hostname)
+	// set incorrect ip
+	ip := net.ParseIP("192.168.101.1")
+
+	verifyTests := []struct {
+		name     string
+		file     File
+		verify   bool
+		function function
+		log      string
+	}{
+		{
+			name: "returns true if ip is same as the current machine",
+			file: File{
+				smbName: "file.txt",
+				fanIP:   ips[0],
+			},
+			verify:   true,
+			function: File.verifyIP(file, ips[0], testLogger),
+			log:      "file.txt ip:" + file.fanIP.String() + " matches comparison ip:" + ips[0].String(),
+		},
+	}
+
+}*/
