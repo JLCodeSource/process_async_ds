7b2402fccd946d83eb5e8c46fd0591f4f06da78c
JLCodeSource <JLCodeSource@gmail.com>
2023-02-28 20:54:07 +0000

    Changed methods to *File pointers vs File

--- verifier.go
+++ verifier.go
@@ -9,7 +9,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func (f File) verifyIP(ip net.IP, logger *logrus.Logger) bool {
+func (f *File) verifyIP(ip net.IP, logger *logrus.Logger) bool {
 	if reflect.DeepEqual(f.fanIP, ip) {
 		logger.Info(f.smbName + " ip:" + f.fanIP.String() + " matches comparison ip:" + ip.String())
 	} else {
@@ -19,7 +19,7 @@ func (f File) verifyIP(ip net.IP, logger *logrus.Logger) bool {
 	return reflect.DeepEqual(f.fanIP, ip)
 }
 
-func (f File) verifyTimeLimit(limit time.Time, logger *logrus.Logger) bool {
+func (f *File) verifyTimeLimit(limit time.Time, logger *logrus.Logger) bool {
 	if f.createTime.After(limit) {
 		logger.Info(f.smbName + " createTime:" + f.createTime.String() + " is after timelimit:" + limit.String())
 	} else {
@@ -28,7 +28,7 @@ func (f File) verifyTimeLimit(limit time.Time, logger *logrus.Logger) bool {
 	return f.createTime.After(limit)
 }
 
-func (f File) verifyInProcessedDataset(datasetID string, logger *logrus.Logger) bool {
+func (f *File) verifyInProcessedDataset(datasetID string, logger *logrus.Logger) bool {
 	if f.datasetID == datasetID {
 		logger.Info(f.smbName + " datasetID:" + f.datasetID + " matches asyncProcessedDatasetID:" + datasetID)
 	} else {
@@ -38,7 +38,7 @@ func (f File) verifyInProcessedDataset(datasetID string, logger *logrus.Logger) 
 	return f.datasetID == datasetID
 }
 
-func (f File) verifyExists(fsys fs.FS, logger *logrus.Logger) bool {
+func (f *File) verifyExists(fsys fs.FS, logger *logrus.Logger) bool {
 	_, err := fs.Stat(fsys, f.stagingPath)
 	if err != nil {
 		logger.Warn(f.smbName + " does not exist at " + f.stagingPath + "; skipping file")
