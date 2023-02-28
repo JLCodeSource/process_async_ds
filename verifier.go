e81c7157aea239ddc73024a6d173484621682c7d
JLCodeSource <JLCodeSource@gmail.com>
2023-02-28 13:46:07 +0000

    Added verfiyIP logging checks

--- verifier.go
+++ verifier.go
@@ -3,8 +3,16 @@ package main
 import (
 	"net"
 	"reflect"
+
+	"github.com/sirupsen/logrus"
 )
 
-func (f File) verifyIP(ip net.IP) bool {
+func (f File) verifyIP(ip net.IP, logger *logrus.Logger) bool {
+	if reflect.DeepEqual(f.fanIP, ip) {
+		logger.Info(f.smbName + " ip:" + f.fanIP.String() + " matches comparison ip:" + ip.String())
+	} else {
+		logger.Warn(f.smbName + " ip:" + f.fanIP.String() + " does not match comparison ip:" + ip.String() + "; skipping file")
+
+	}
 	return reflect.DeepEqual(f.fanIP, ip)
 }
