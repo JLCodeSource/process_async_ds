f50afe5b383af65482f04d15e498c670645a69da
JLCodeSource <JLCodeSource@gmail.com>
2023-03-01 11:41:48 +0000

    Made logging  strings into consts

--- verifier.go
+++ verifier.go
@@ -11,15 +11,23 @@ import (
 )
 
 const (
-	fSizeMatchTrueLog  = "%v file.size:%v matches size in file.stagingPath size:%v"
-	fSizeMatchFalseLog = "%v file.size:%v does not match size in file.stagingPath size:%v; skipping file"
+	fIPMatchTrueLog          = "%v (file.id:%v) file.ip:%v matches comparison ip:%v"
+	fIPMatchFalseLog         = "%v (file.id:%v) file.ip:%v does not match comparison ip:%v; skipping file"
+	fCreateTimeMatchTrueLog  = "%v (file.id:%v) file.createTime:%v is after timelimit:%v"
+	fCreateTimeMatchFalseLog = "%v (file.id:%v) file.createTime:%v is before timelimit:%v; skipping file"
+	fDatasetMatchTrueLog     = "%v (file.id:%v) file.datasetID:%v matches asyncProcessedDataset:%v"
+	fDatasetMatchFalseLog    = "%v (file.id:%v) file.datasetID:%v does not match asyncProcessedDataset:%v; skipping file"
+	fExistsTrueLog           = "%v (file.id:%v) exists at file.stagingPath:%v"
+	fExistsFalseLog          = "%v (file.id:%v) does not exist at file.stagingPath:%v; skipping file"
+	fSizeMatchTrueLog        = "%v (file.id:%v) file.size:%v matches size in file.stagingPath size:%v"
+	fSizeMatchFalseLog       = "%v (file.id:%v) file.size:%v does not match size in file.stagingPath size:%v; skipping file"
 )
 
 func (f *File) verifyIP(ip net.IP, logger *logrus.Logger) bool {
 	if reflect.DeepEqual(f.fanIP, ip) {
-		logger.Info(f.smbName + " ip:" + f.fanIP.String() + " matches comparison ip:" + ip.String())
+		logger.Info(fmt.Sprintf(fIPMatchTrueLog, f.smbName, f.id, f.fanIP, ip))
 	} else {
-		logger.Warn(f.smbName + " ip:" + f.fanIP.String() + " does not match comparison ip:" + ip.String() + "; skipping file")
+		logger.Warn(fmt.Sprintf(fIPMatchFalseLog, f.smbName, f.id, f.fanIP, ip))
 
 	}
 	return reflect.DeepEqual(f.fanIP, ip)
@@ -27,19 +35,28 @@ func (f *File) verifyIP(ip net.IP, logger *logrus.Logger) bool {
 
 func (f *File) verifyTimeLimit(limit time.Time, logger *logrus.Logger) bool {
 	if f.createTime.After(limit) {
-		logger.Info(f.smbName + " createTime:" + f.createTime.String() + " is after timelimit:" + limit.String())
+		logger.Info(fmt.Sprintf(
+			fCreateTimeMatchTrueLog,
+			f.smbName,
+			f.id,
+			f.createTime.Round(time.Millisecond),
+			limit.Round(time.Millisecond)))
 	} else {
-		logger.Warn(f.smbName + " createTime:" + f.createTime.String() + " is before timelimit:" + limit.String() + "; skipping file")
+		logger.Warn(fmt.Sprintf(
+			fCreateTimeMatchFalseLog,
+			f.smbName,
+			f.id,
+			f.createTime.Round(time.Millisecond),
+			limit.Round(time.Millisecond)))
 	}
 	return f.createTime.After(limit)
 }
 
 func (f *File) verifyInProcessedDataset(datasetID string, logger *logrus.Logger) bool {
 	if f.datasetID == datasetID {
-		logger.Info(f.smbName + " datasetID:" + f.datasetID + " matches asyncProcessedDatasetID:" + datasetID)
+		logger.Info(fmt.Sprintf(fDatasetMatchTrueLog, f.smbName, f.id, f.datasetID, datasetID))
 	} else {
-		logger.Warn(f.smbName + " datasetID:" + f.datasetID + " does not match asyncProcessedDatasetID:" + datasetID +
-			"; skipping file")
+		logger.Warn(fmt.Sprintf(fDatasetMatchFalseLog, f.smbName, f.id, f.datasetID, datasetID))
 	}
 	return f.datasetID == datasetID
 }
@@ -47,19 +64,19 @@ func (f *File) verifyInProcessedDataset(datasetID string, logger *logrus.Logger)
 func (f *File) verifyExists(fsys fs.FS, logger *logrus.Logger) bool {
 	_, err := fs.Stat(fsys, f.stagingPath)
 	if err != nil {
-		logger.Warn(f.smbName + " does not exist at " + f.stagingPath + "; skipping file")
+		logger.Warn(fmt.Sprintf(fExistsFalseLog, f.smbName, f.id, f.stagingPath))
 		return false
 	}
-	logger.Info(f.smbName + " exists at " + f.stagingPath)
+	logger.Info(fmt.Sprintf(fExistsTrueLog, f.smbName, f.id, f.stagingPath))
 	return true
 }
 
 func (f *File) verifyFileSize(fsys fs.FS, logger *logrus.Logger) bool {
 	info, _ := fs.Stat(fsys, f.stagingPath)
 	if info.Size() != f.fileInfo.Size() {
-		logger.Warn(fmt.Sprintf(fSizeMatchFalseLog, f.smbName, f.size, f.fileInfo.Size()))
+		logger.Warn(fmt.Sprintf(fSizeMatchFalseLog, f.smbName, f.id, f.size, f.fileInfo.Size()))
 		return false
 	}
-	logger.Info(fmt.Sprintf(fSizeMatchTrueLog, f.smbName, f.size, f.fileInfo.Size()))
+	logger.Info(fmt.Sprintf(fSizeMatchTrueLog, f.smbName, f.id, f.size, f.fileInfo.Size()))
 	return true
 }
