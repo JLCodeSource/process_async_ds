8babb2c135c3625d5b3870b0f749b506355b7df1
JLCodeSource <JLCodeSource@gmail.com>
2023-03-18 01:39:33 +0000

    Added Move test & log

--- mover.go
+++ mover.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
-func (f *File) UpdatePath() {
+const (
+	fMoveFileLog = "%v: (file.id:%v) oldPath:%v, newPath:%v"
+)
+
+func (f *File) UpdatePath() string {
 	oldDir, fn := path.Split(f.stagingPath)
 	parts := strings.Split(oldDir, string(os.PathSeparator))
 	lastParts := parts[2:]
 	firstParts := parts[:2]
 	fp := strings.Join(firstParts, string(os.PathSeparator))
 	lp := strings.Join(lastParts, string(os.PathSeparator))
-	f.stagingPath = fp + ".processed" + string(os.PathSeparator) + lp + fn
-	return
+	return fp + ".processed" + string(os.PathSeparator) + lp + fn
+}
+
+func (f *File) Move(newLocation string, logger *logrus.Logger) {
+	oldLocation := f.stagingPath
+	_ = os.Rename(oldLocation, newLocation)
+	//if err != nil {
+	//	logger.Fatal(err)
+	//}
+	logger.Info(fmt.Sprintf(fMoveFileLog, f.smbName, f.id, oldLocation, newLocation))
 }
--- mover_test.go
+++ mover_test.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"strings"
 	"testing"
-	"testing/fstest"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestMover(t *testing.T) {
-	var files []File
-	fsys = fstest.MapFS{}
+func TestUpdatePath(t *testing.T) {
 	fsys, files = createFSTest(10)
 	t.Run("should change path to xxx.processed", func(t *testing.T) {
 		for _, f := range files {
@@ -18,11 +18,37 @@ func TestMover(t *testing.T) {
 			parts := strings.Split(oldDir, string(os.PathSeparator))
 			lastParts := parts[2:]
 			firstParts := parts[:2]
-			f.UpdatePath()
+			got := f.UpdatePath()
 			fp := strings.Join(firstParts, string(os.PathSeparator))
 			lp := strings.Join(lastParts, string(os.PathSeparator))
 			want := fp + ".processed" + string(os.PathSeparator) + lp + fn
-			assertCorrectString(t, f.stagingPath, want)
+			assertCorrectString(t, got, want)
 		}
 	})
+}
+
+func TestMoveFile(t *testing.T) {
+	fsys, files = createFSTest(10)
+	t.Run("should move file to new path & log it", func(t *testing.T) {
+		for _, f := range files {
+			testLogger, hook = setupLogs()
+			oldPath := f.stagingPath
+			newPath := f.UpdatePath()
+			f.Move(newPath, testLogger)
+			assert.NotEqual(t, oldPath, newPath)
+			_, err := fsys.Stat(f.stagingPath)
+			assert.Nil(t, err)
+
+			gotLogMsg := hook.LastEntry().Message
+			wantLogMsg := fmt.Sprintf(fMoveFileLog,
+				f.smbName,
+				f.id,
+				oldPath,
+				newPath)
+
+			assertCorrectString(t, gotLogMsg, wantLogMsg)
+
+		}
+	})
+
 }
