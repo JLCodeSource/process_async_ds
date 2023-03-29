efa71ac0c397f638bec272a588f13bb4e75ad100
JLCodeSource <JLCodeSource@gmail.com>
2023-03-29 22:29:22 +0000

    Add todo comments to ap.processFiles

--- worker.go
+++ worker.go
@@ -5,12 +5,17 @@ func (ap *asyncProcessor) processFiles() {
 	for i := range *ap.Files {
 		(*ap.Files)[i].Hasher()
 		(*ap.Files)[i].oldHash = (*ap.Files)[i].hash
+		// log
 		(*ap.Files)[i].oldStagingPath = (*ap.Files)[i].stagingPath
+		// log
 		(*ap.Files)[i].Move()
+		// log (in Move)
 		(*ap.Files)[i].Hasher()
 		if (*ap.Files)[i].compareHashes() {
 			(*ap.Files)[i].success = true
-		}
+			// log
+		} // Add fail & log
+		// log result
 	}
 }
 
