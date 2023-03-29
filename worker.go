9509e3a638a2435f2cab9147b2f417acca513abd
JLCodeSource <JLCodeSource@gmail.com>
2023-03-29 19:55:02 +0000

    Update worker Hasher to match f.Hasher

--- worker.go
+++ worker.go
@@ -3,8 +3,8 @@ package main
 func (ap *asyncProcessor) processFiles() {
 	e = ap.getEnv()
 	for i := range *ap.Files {
-		(*ap.Files)[i].hasher(e.afs, e.logger)
+		(*ap.Files)[i].Hasher()
 		(*ap.Files)[i].Move(e.afs, e.logger)
-		(*ap.Files)[i].hasher(e.afs, e.logger)
+		(*ap.Files)[i].Hasher()
 	}
 }
