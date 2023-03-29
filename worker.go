59aef22e579818a54779ecfa0d563442b9b4c190
JLCodeSource <JLCodeSource@gmail.com>
2023-03-29 21:54:49 +0000

    Test compare oldSP/newSP vs old/newHashes

--- worker.go
+++ worker.go
@@ -4,7 +4,11 @@ func (ap *asyncProcessor) processFiles() {
 	e = ap.getEnv()
 	for i := range *ap.Files {
 		(*ap.Files)[i].Hasher()
+		(*ap.Files)[i].oldStagingPath = (*ap.Files)[i].stagingPath
 		(*ap.Files)[i].Move()
 		(*ap.Files)[i].Hasher()
+		// Compare hashes
+		// Confirm success
+		// Set success bool on File
 	}
 }
--- worker_test.go
+++ worker_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/sha256"
 	"testing"
 
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -17,10 +19,18 @@ func TestWorker(t *testing.T) {
 
 		var oldPaths []string
 		var newPaths []string
+		var oldHashes []string
 
 		for i := range files {
 			oldPaths = append(oldPaths, files[i].stagingPath)
 			newPaths = append(newPaths, newPath(&files[i]))
+			content, err := afero.ReadFile(afs, files[i].stagingPath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			b := sha256.Sum256(content)
+			s := string(b[:])
+			oldHashes = append(oldHashes, s)
 		}
 
 		ap.processFiles()
@@ -28,6 +38,8 @@ func TestWorker(t *testing.T) {
 		for i := range oldPaths {
 			assert.NotEqual(t, oldPaths[i], newPaths[i])
 			assert.Equal(t, newPaths[i], files[i].stagingPath)
+			assert.Equal(t, oldPaths[i], files[i].oldStagingPath)
+			assert.Equal(t, oldHashes[i], string(files[i].hash[:]))
 		}
 
 	})
