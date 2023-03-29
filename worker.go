85b8c956a9c710a074a8116bc947ce4577e15be2
JLCodeSource <JLCodeSource@gmail.com>
2023-03-29 19:28:46 +0000

    Add worker.processFiles

--- worker.go
+++ worker.go
@@ -1,1 +1,10 @@
 package main
+
+func (ap *asyncProcessor) processFiles() {
+	e = ap.getEnv()
+	for i := range *ap.Files {
+		(*ap.Files)[i].hasher(e.afs, e.logger)
+		(*ap.Files)[i].Move(e.afs, e.logger)
+		(*ap.Files)[i].hasher(e.afs, e.logger)
+	}
+}
--- worker_test.go
+++ worker_test.go
@@ -1,9 +1,26 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestWorker(t *testing.T) {
-	t.Run("it should take a parsedLine ", func(t *testing.T) {
+	t.Run("given a list of 1 file, it processes it", func(t *testing.T) {
+		afs, files := createAferoTest(t, 1, false)
+		e = new(env)
 
+		e.logger, hook = setupLogs()
+		e.afs = afs
+		ap = NewAsyncProcessor(e, &files)
+
+		oldPath := files[0].stagingPath
+		newPath := newPath(&files[0])
+
+		ap.processFiles()
+
+		assert.NotEqual(t, oldPath, newPath)
+		assert.Equal(t, newPath, files[0].stagingPath)
 	})
 }
