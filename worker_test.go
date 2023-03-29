33f881044f58c7d3d3c966c55fca4b9de37fc4b4
JLCodeSource <JLCodeSource@gmail.com>
2023-03-29 19:41:20 +0000

    Add TestWorker support for multiple files

--- worker_test.go
+++ worker_test.go
@@ -7,20 +7,28 @@ import (
 )
 
 func TestWorker(t *testing.T) {
-	t.Run("given a list of 1 file, it processes it", func(t *testing.T) {
-		afs, files := createAferoTest(t, 1, false)
+	t.Run("given a list of multiple files, it processes them", func(t *testing.T) {
+		afs, files := createAferoTest(t, 10, false)
 		e = new(env)
 
 		e.logger, hook = setupLogs()
 		e.afs = afs
 		ap = NewAsyncProcessor(e, &files)
 
-		oldPath := files[0].stagingPath
-		newPath := newPath(&files[0])
+		var oldPaths []string
+		var newPaths []string
+
+		for i := range files {
+			oldPaths = append(oldPaths, files[i].stagingPath)
+			newPaths = append(newPaths, newPath(&files[i]))
+		}
 
 		ap.processFiles()
 
-		assert.NotEqual(t, oldPath, newPath)
-		assert.Equal(t, newPath, files[0].stagingPath)
+		for i := range oldPaths {
+			assert.NotEqual(t, oldPaths[i], newPaths[i])
+			assert.Equal(t, newPaths[i], files[i].stagingPath)
+		}
+
 	})
 }
